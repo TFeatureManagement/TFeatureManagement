6b8dc73bc277765bb5068cbb659b3900ba8e20c1
brental <10455806+brental@users.noreply.github.com>
2021-05-29 21:23:42 +1000

    Added tests for FeatureCleanupDateAttribute.

---
+++ test/TFeatureManagement.Tests/DataAnnotations/FeatureCleanupDateAttributeTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TFeatureManagement.DataAnnotations;
+
+namespace TFeatureManagement.Tests.DataAnnotations
+{
+    [TestClass]
+    public class FeatureCleanupDateAttributeTests
+    {
+        private FeatureCleanupDateAttribute _underTest;
+
+        public void Constructor_SpecifiedYearMonthAndDayAreAValidDate_CleanupDateValueSetToStringRepresentationOfCleanupDate()
+        {
+            // Arrange
+            var expectedCleanupDate = new DateTime(2000, 1, 1);
+            _underTest = new FeatureCleanupDateAttribute(expectedCleanupDate.Year, expectedCleanupDate.Month, expectedCleanupDate.Day);
+
+            // Act
+            var cleanupDateValue = _underTest.CleanupDateValue;
+
+            // Assert
+            cleanupDateValue.Should().Be($"{expectedCleanupDate.Year}-{expectedCleanupDate.Month}-{expectedCleanupDate.Day}");
+        }
+
+        public void Constructor_SpecifiedYearMonthAndDayAreAValidDate_CleanupDateSetToDate()
+        {
+            // Arrange
+            var expectedCleanupDate = new DateTime(2000, 1, 1);
+            _underTest = new FeatureCleanupDateAttribute(expectedCleanupDate.Year, expectedCleanupDate.Month, expectedCleanupDate.Day);
+
+            // Act
+            var cleanupDate = _underTest.CleanupDate;
+
+            // Assert
+            cleanupDate.Should().BeSameDateAs(expectedCleanupDate.Date);
+        }
+
+        public void Constructor_SpecifiedYearMonthAndDayAreNotAValidDate_CleanupDateValueSetToStringRepresentationOfCleanupDate()
+        {
+            // Arrange
+            _underTest = new FeatureCleanupDateAttribute(2000, 01, 99);
+
+            // Act
+            var cleanupDateValue = _underTest.CleanupDateValue;
+
+            // Assert
+            cleanupDateValue.Should().Be($"{2000}-{01}-{99}");
+        }
+
+        public void Constructor_SpecifiedYearMonthAndDayAreNotAValidDate_CleanupDateSetToNull()
+        {
+            // Arrange
+            _underTest = new FeatureCleanupDateAttribute(2000, 01, 99);
+
+            // Act
+            var cleanupDate = _underTest.CleanupDate;
+
+            // Assert
+            cleanupDate.Should().BeNull();
+        }
+    }
+}
