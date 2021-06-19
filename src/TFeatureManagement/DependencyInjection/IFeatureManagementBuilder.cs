d40c271a1f7f74cbe04d9e1705b5fdb7eee63b46
brental <10455806+brental@users.noreply.github.com>
2021-06-19 20:50:52 +1000

    Added FeatureFilterBase class for feature filters with a feature enum type.

--- src/TFeatureManagement/DependencyInjection/IFeatureManagementBuilder.cs
+++ src/TFeatureManagement/DependencyInjection/IFeatureManagementBuilder.cs
@@ -11,13 +11,15 @@ namespace TFeatureManagement.DependencyInjection
         IServiceCollection Services { get; }
 
         /// <inheritdoc cref="IFeatureManagementBuilder.AddFeatureFilter{T}" />
-        IFeatureManagementBuilder<TFeature> AddFeatureFilter<T>() where T : IFeatureFilterMetadata;
+        IFeatureManagementBuilder<TFeature> AddFeatureFilter<T>()
+            where T : IFeatureFilterMetadata;
 
         /// <summary>
         /// Adds an <see cref="ISessionManager{TFeature}" /> to be used for storing feature state in a session.
         /// </summary>
         /// <typeparam name="T">An implementation of <see cref="ISessionManager{TFeature}" />.</typeparam>
         /// <returns>The feature management builder.</returns>
-        IFeatureManagementBuilder<TFeature> AddSessionManager<T>() where T : ISessionManager<TFeature>;
+        IFeatureManagementBuilder<TFeature> AddSessionManager<T>()
+            where T : ISessionManager<TFeature>;
     }
 }
