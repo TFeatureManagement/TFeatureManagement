191729ca745fd8f93a1f6377f92192629e3f39b3
brental <10455806+brental@users.noreply.github.com>
2021-06-19 21:07:49 +1000

    Added ContextFeatureFilterBase for contextual feature filters with a feature enum type.

---
+++ src/TFeatureManagement/ContextualFeatureFilterBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.FeatureManagement;
+using System;
+using System.Threading.Tasks;
+
+namespace TFeatureManagement
+{
+    /// <summary>
+    /// Base class for contextual feature filters with a feature enum type.
+    /// </summary>
+    /// <typeparam name="TFeature">The feature enum type.</typeparam>
+    /// <typeparam name="TContext">The context type.</typeparam>
+    public abstract class ContextualFeatureFilterBase<TFeature, TContext> : IContextualFeatureFilter<TFeature, TContext>
+        where TFeature : Enum
+    {
+        private readonly IEnumParser<TFeature> _enumParser;
+
+        /// <summary>
+        /// Creates a contextual feature filter with an enum parser to convert a feature name to its matching value in
+        /// the specified feature enum.
+        /// </summary>
+        /// <param name="enumParser">The enum parser for the feature enum type.</param>
+        protected ContextualFeatureFilterBase(IEnumParser<TFeature> enumParser)
+        {
+            _enumParser = enumParser;
+        }
+
+        /// <inheritdoc cref="EvaluateAsync(FeatureFilterEvaluationContext{TFeature}, TContext)" />
+        public Task<bool> EvaluateAsync(FeatureFilterEvaluationContext context, TContext appContext)
+        {
+            var feature = _enumParser.Parse(context.FeatureName);
+
+            return EvaluateAsync(new FeatureFilterEvaluationContext<TFeature>
+            {
+                Feature = feature,
+                Parameters = context.Parameters
+            }, appContext);
+        }
+
+        /// <inheritdoc />
+        public abstract Task<bool> EvaluateAsync(FeatureFilterEvaluationContext<TFeature> context, TContext appContext);
+    }
+}
