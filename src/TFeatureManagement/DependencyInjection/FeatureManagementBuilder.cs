030e33f2d9846cd4869dfdccd184c0be7ff1083d
brental <10455806+brental@users.noreply.github.com>
2021-06-20 13:10:43 +1000

    Improved feature filter implementation checking in AddFeatureFilter (along with unit tests).

--- src/TFeatureManagement/DependencyInjection/FeatureManagementBuilder.cs
+++ src/TFeatureManagement/DependencyInjection/FeatureManagementBuilder.cs
@@ -5,7 +5,8 @@ using System.Linq;
 
 namespace TFeatureManagement.DependencyInjection
 {
-    public class FeatureManagementBuilder<TFeature> : IFeatureManagementBuilder<TFeature>
+    /// <inheritdoc cref="IFeatureManagementBuilder{TFeature}" />
+    internal class FeatureManagementBuilder<TFeature> : IFeatureManagementBuilder<TFeature>
         where TFeature : Enum
     {
         private readonly IFeatureManagementBuilder _baseFeatureManagementBuilder;
@@ -15,24 +16,37 @@ namespace TFeatureManagement.DependencyInjection
             _baseFeatureManagementBuilder = baseFeatureManagementBuilder;
         }
 
+        /// <inheritdoc />
         public IServiceCollection Services => _baseFeatureManagementBuilder.Services;
 
+        /// <inheritdoc />
         public IFeatureManagementBuilder<TFeature> AddFeatureFilter<T>()
             where T : IFeatureFilterMetadata
         {
             var implementationType = typeof(T);
-            if (implementationType.GetInterfaces()
-                .Any(i => i.IsGenericType
-                          && i.GetGenericTypeDefinition().IsAssignableFrom(typeof(IFeatureFilterMetadata<>))
-                          && i.GetGenericArguments()[0] != typeof(TFeature)))
+
+            var featureFilterImplementations = implementationType.GetInterfaces()
+                .Where(i => i == typeof(IFeatureFilter) ||
+                            (i.IsGenericType && i.GetGenericTypeDefinition().IsAssignableFrom(typeof(IContextualFeatureFilter<>))) ||
+                            (i.IsGenericType && i.GetGenericTypeDefinition().IsAssignableFrom(typeof(IFeatureFilterMetadata<>))));
+
+            if (featureFilterImplementations.Count() > 1)
             {
-                throw new ArgumentException($"The feature enum type specified by '{typeof(T)}' must match the feature enum type of the feature management builder.", nameof(T));
+                throw new ArgumentException("A single feature filter cannot implement more than one feature filter interface.", nameof(T));
+            }
+            else if (featureFilterImplementations
+                .Any(i => i.IsGenericType &&
+                          i.GetGenericTypeDefinition().IsAssignableFrom(typeof(IFeatureFilterMetadata<>)) &&
+                          i.GetGenericArguments()[0] != typeof(TFeature)))
+            {
+                throw new ArgumentException("The feature enum type specified by the feature filter must match the feature enum type of the feature management builder.", nameof(T));
             }
 
             _baseFeatureManagementBuilder.AddFeatureFilter<T>();
             return this;
         }
 
+        /// <inheritdoc />
         public IFeatureManagementBuilder<TFeature> AddSessionManager<T>()
             where T : ISessionManager<TFeature>
         {
@@ -41,6 +55,7 @@ namespace TFeatureManagement.DependencyInjection
             return this;
         }
 
+        /// <inheritdoc />
         public IFeatureManagementBuilder<TFeature> AddFeatureCleanup()
         {
             _baseFeatureManagementBuilder.Services.AddSingleton<IFeatureCleanupManager<TFeature>, FeatureCleanupManager<TFeature>>();
---
+++ test/TFeatureManagement.Tests/FeatureManagementBuilderTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.FeatureManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using TFeatureManagement.DependencyInjection;
+
+namespace TFeatureManagement.Tests
+{
+    [TestClass]
+    public class FeatureManagementBuilderTests
+    {
+        private FeatureManagementBuilder<Feature> _underTest;
+
+        private Mock<IFeatureManagementBuilder> _baseFeatureManagementBuilder;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _baseFeatureManagementBuilder = new Mock<IFeatureManagementBuilder>();
+
+            _underTest = new FeatureManagementBuilder<Feature>(_baseFeatureManagementBuilder.Object);
+        }
+
+        [TestMethod]
+        public void AddFeatureFilter_FeatureFilterImplementsMultipleFeatureFilterInterfaces_ThrowsArgumentException()
+        {
+            // Arrange and Act
+            Action action = () => _underTest.AddFeatureFilter<FeatureFilterWithMultipleFeatureFilterInterfaces>();
+
+            // Assert
+            action.Should()
+                .Throw<ArgumentException>()
+                .Where(ex => ex.Message.StartsWith("A single feature filter cannot implement more than one feature filter interface.") &&
+                             ex.ParamName.Equals("T"));
+        }
+
+        [TestMethod]
+        public void AddFeatureFilter_FeatureFilterWithFeatureEnumTypeThatDoesNotMatchBuilderFeatureEnumType_ThrowsArgumentException()
+        {
+            // Arrange and Act
+            Action action = () => _underTest.AddFeatureFilter<FeatureFilterForOtherFeature>();
+
+            // Assert
+            action.Should()
+                .Throw<ArgumentException>()
+                .Where(ex => ex.Message.StartsWith("The feature enum type specified by the feature filter must match the feature enum type of the feature management builder.") &&
+                             ex.ParamName.Equals("T"));
+        }
+
+        [TestMethod]
+        public void AddFeatureFilter_FeatureFilterWithFeatureEnumTypeThatDoesMatchBuilderFeatureEnumType_CallsBaseAddFeatureFilter()
+        {
+            // Arrange and Act
+            _underTest.AddFeatureFilter<FeatureFilter>();
+
+            // Assert
+            _baseFeatureManagementBuilder.Verify(x => x.AddFeatureFilter<FeatureFilter>(), Times.Once);
+        }
+
+        [TestMethod]
+        public void AddFeatureFilter_FeatureFilterWithoutFeatureEnumType_CallsBaseAddFeatureFilter()
+        {
+            // Arrange and Act
+            _underTest.AddFeatureFilter<FeatureFilterWithoutFeatureEnumType>();
+
+            // Assert
+            _baseFeatureManagementBuilder.Verify(x => x.AddFeatureFilter<FeatureFilterWithoutFeatureEnumType>(), Times.Once);
+        }
+    }
+}
