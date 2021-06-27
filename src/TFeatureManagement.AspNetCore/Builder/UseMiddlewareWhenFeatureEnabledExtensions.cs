bef40c2e489c8db7094d1211178515f83bf3c602
brental <10455806+brental@users.noreply.github.com>
2021-06-27 15:57:55 +1000

    Renamed UseForFeature and UseMiddlewareForFeature extensions to UseWhenFeatureEnabled and UseMiddlewareWhenFeatureEnabled to follow the ASP.NET Core UseWhen naming for conditional branches / middleware.

---
+++ src/TFeatureManagement.AspNetCore/Builder/UseMiddlewareWhenFeatureEnabledExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+
+namespace TFeatureManagement.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods for adding typed middleware that require a feature to be enabled for the middleware to
+    /// execute.
+    /// </summary>
+    public static class UseMiddlewareWhenFeatureEnabledExtensions
+    {
+        /// <summary>
+        /// Conditionally adds a middleware type to the application's request pipeline.
+        /// </summary>
+        /// <inheritdoc cref="UseMiddlewareExtensions.UseMiddleware{TMiddleware}(IApplicationBuilder, object[])" />
+        /// <typeparam name="TFeature">The feature enum type.</typeparam>
+        /// <param name="feature">The feature that is required to be enabled for the middleware to execute.</param>
+        public static IApplicationBuilder UseMiddlewareWhenFeatureEnabled<TMiddleware, TFeature>(this IApplicationBuilder app, TFeature feature, params object[] args)
+            where TFeature : Enum
+        {
+            return app.UseMiddlewareWhenFeatureEnabled(feature, typeof(TMiddleware), args);
+        }
+
+        /// <summary>
+        /// Conditionally adds a middleware type to the application's request pipeline.
+        /// </summary>
+        /// <inheritdoc cref="UseMiddlewareExtensions.UseMiddleware(IApplicationBuilder, Type, object[])" />
+        /// <typeparam name="TFeature">The feature enum type.</typeparam>
+        /// <param name="feature">The feature that is required to be enabled for the middleware to execute.</param>
+        public static IApplicationBuilder UseMiddlewareWhenFeatureEnabled<TFeature>(this IApplicationBuilder app, TFeature feature, Type middleware, params object[] args)
+            where TFeature : Enum
+        {
+            return app.UseWhenFeatureEnabled(feature, builder =>
+            {
+                builder.UseMiddleware(middleware, args);
+            });
+        }
+    }
+}
