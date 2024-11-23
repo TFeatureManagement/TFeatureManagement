86d754a667cfc8d34a600b9c4d7d209280537816
brental <10455806+brental@users.noreply.github.com>
2024-11-23 18:10:47 +1100

    Suppress test warnings.

--- tests/TFeatureManagement.Tests/FeatureManagerTests.cs
+++ tests/TFeatureManagement.Tests/FeatureManagerTests.cs
@@ -68,7 +68,9 @@ public class FeatureManagerTests
 
         // Assert
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning disable CA2012 // Use ValueTasks correctly
         _baseFeatureManager.Received().IsEnabledAsync(expectedFeatureName);
+#pragma warning restore CA2012 // Use ValueTasks correctly
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
     }
 
@@ -101,7 +103,9 @@ public class FeatureManagerTests
 
         // Assert
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+#pragma warning disable CA2012 // Use ValueTasks correctly
         _baseFeatureManager.Received().IsEnabledAsync(expectedFeatureName, expectedContext, expectedCancellationToken);
+#pragma warning restore CA2012 // Use ValueTasks correctly
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
     }
 
