8167573d90f3b6df0b7274248a4b34f3edad5c80
brental <10455806+brental@users.noreply.github.com>
2024-12-03 17:42:17 +1100

    Update to Microsoft.FeatureManagement v4 and add feature variant support. (#116)
    Closes #49

---
+++ src/TFeatureManagement/FeatureVariantManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace TFeatureManagement;
+
+public sealed class FeatureVariantManager<TFeature> : IFeatureVariantManager<TFeature>
+    where TFeature : struct, Enum
+{
+    private readonly IVariantFeatureManager _baseFeatureVariantManager;
+    private readonly IFeatureNameProvider<TFeature> _featureNameProvider;
+
+    public FeatureVariantManager(
+        IVariantFeatureManager baseFeatureVariantManager,
+        IFeatureNameProvider<TFeature> featureNameProvider)
+    {
+        _baseFeatureVariantManager = baseFeatureVariantManager ?? throw new ArgumentNullException(nameof(baseFeatureVariantManager));
+        _featureNameProvider = featureNameProvider ?? throw new ArgumentNullException(nameof(featureNameProvider));
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<FeatureVariant?> GetVariantAsync(TFeature feature, CancellationToken cancellationToken = default)
+    {
+        var variant = await _baseFeatureVariantManager.GetVariantAsync(_featureNameProvider.GetFeatureName(feature), cancellationToken).ConfigureAwait(false);
+
+        return variant != null ? new FeatureVariant(variant.Name, variant.Configuration) : null;
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<FeatureVariant?> GetVariantAsync(TFeature feature, ITargetingContext context, CancellationToken cancellationToken = default)
+    {
+        var targetingContext = new Microsoft.FeatureManagement.FeatureFilters.TargetingContext
+        {
+            UserId = context.UserId,
+            Groups = context.Groups
+        };
+
+        var variant = await _baseFeatureVariantManager.GetVariantAsync(_featureNameProvider.GetFeatureName(feature), targetingContext, cancellationToken).ConfigureAwait(false);
+
+        return variant != null ? new FeatureVariant(variant.Name, variant.Configuration) : null;
+    }
+}
---
+++ src/TFeatureManagement/TargetingContext.cs
@@ -0,0 +1,8 @@
+﻿namespace TFeatureManagement;
+
+public class TargetingContext : ITargetingContext
+{
+    public string? UserId { get; set; }
+
+    public IEnumerable<string>? Groups { get; set; }
+}
